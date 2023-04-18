4070baf11fb889ed675202235c6ea897ff2d9056
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-04-18 08:36:08 +0000

    adding js code

--- app.js
+++ app.js
@@ -8,40 +8,47 @@ function Employee(id, fullName, department, level, imageUrl) {
     this.salary = this.calculateSalary();
 }
 
-//4. Define the calculateSalary method on the Employee prototype
+Employee.prototype.employee_info = function () {
+    let info = `Employee's name is ${this.fullName} and their salary is ${this.salary}`;
+    return info;
+}
+
+// Define the calculateSalary method on the Employee prototype
 Employee.prototype.calculateSalary = function () {
-    // Salary table
     var salaryTable = {
         Junior: { min: 500, max: 1000 },
         Mid_Senior: { min: 1000, max: 1500 },
         Senior: { min: 1500, max: 2000 }
     };
-
-    // Get the minimum and maximum salary for the employee's level
     var levelSalary = salaryTable[this.level];
     var minSalary = levelSalary.min;
     var maxSalary = levelSalary.max;
-
-    // Generate a random salary between the min and max for the employee's level
     var randomSalary = Math.floor(Math.random() * (maxSalary - minSalary + 1)) + minSalary;
-
-    // Calculate the net salary after applying a 7.5% tax
     var netSalary = randomSalary * 0.925;
-
     return netSalary;
 };
 
-// 5.Create instances of the Employee constructor function for each employee
+// Create instances of the Employee constructor function for each employee
 var employee1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "https://example.com/ghazi.jpg");
 var employee2 = new Employee(1001, "Lana Ali", "Finance", "Senior", "https://example.com/lana.jpg");
 var employee3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "https://example.com/tamara.jpg");
 var employee4 = new Employee(1003, "Safi Walid", "Administration", "Mid_Senior", "https://example.com/safi.jpg");
-var employee5 = new Employee(1004, "Omar Zaid", "Development", "Senior", "https://example.com/omar.jpg");
-var employee6 = new Employee(1005, "Rana Saleh", "Development", "Junior", "https://example.com/rana.jpg");
-var employee7 = new Employee(1006, "Hadi Ahmad", "Finance", "Mid_Senior", "https://example.com/hadi.jpg");
 
 
-console.log(employee1);
-console.log(employee2);
-console.log(employee3);
-console.log(employee4);
+
+const employee1Par = document.createElement('p');
+const employee2Par = document.createElement('p');
+const employee3Par = document.createElement('p');
+const employee4Par = document.createElement('p');
+
+employee1Par.textContent = employee1.employee_info();
+employee2Par.textContent = employee2.employee_info();
+employee3Par.textContent = employee3.employee_info();
+employee4Par.textContent = employee4.employee_info();
+
+const parentEle = document.getElementById('emp');
+parentEle.appendChild(employee1Par);
+parentEle.appendChild(employee2Par);
+parentEle.appendChild(employee3Par);
+parentEle.appendChild(employee4Par);
+
